8919bc206a070ccfb01c9a665b16ddd9d4e80c66
BangOns <syahr9950@gmail.com>
2024-03-14 22:47:40 +0700

    fix: Create Disabled in Button Bahan

--- alterra-react-router/src/pages/BurgerOrder/Component/Bahan.jsx
+++ alterra-react-router/src/pages/BurgerOrder/Component/Bahan.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 export default function Bahan({ isMax, items, Click }) {
+  console.log(isMax);
   return (
     <button
       disabled={isMax}
-      className={`w-28 px-3 py-1 rounded-md text-lg text-center cursor-pointer  bg-amber-400 text-black shadow-md hover:bg-amber-300  ${
-        isMax ? "cursor-not-allowed" : ""
-      } `}
+      className={`w-28 px-3 py-1 rounded-md text-lg text-center cursor-pointer  bg-amber-400 text-black shadow-md hover:bg-amber-300 disabled:cursor-not-allowed disabled:hover:bg-amber-400 `}
       key={items.id}
       onClick={() => {
         Click(items, "add");
